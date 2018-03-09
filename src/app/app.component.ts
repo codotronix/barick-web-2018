99e7a429469c652f79dc6d7d9b3b09393b3d2086
Suman Barick <codotronix@gmail.com>
2018-03-09 23:21:30 +0530

    listcard in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,21 +15,21 @@ export class AppComponent {
                 {
                     name: "Beware of Boxes",
                     desc: "Demo game created with my game framework CXGF",
-                    tag: ["HTML", "CSS", "JavaScript", "CXGF - Codotronix Game Framework", "Game"],
+                    tags: ["HTML", "CSS", "JavaScript", "CXGF - Codotronix Game Framework", "Game"],
                     img: "",
                     url: ""
                 },
                 {
                     name: "Tankgular",
                     desc: "A tank battle game to demonstrate the browser responsiveness of AngularJS, where each tank is an angular directive.",
-                    tag: ["AngularJS", "HTML", "CSS", "JavaScript", "Game"],
+                    tags: ["AngularJS", "HTML", "CSS", "JavaScript", "Game"],
                     img: "",
                     url: ""
                 },
                 {
                     name: "Asteroid Attack",
                     desc: "A simple browser based game where the player needs to shoot down all the asteroids to save the day",
-                    tag: ["HTML", "CSS", "JavaScript", "Game"],
+                    tags: ["HTML", "CSS", "JavaScript", "Game"],
                     img: "",
                     url: ""
                 }
@@ -42,28 +42,28 @@ export class AppComponent {
                 {
                     name: "Web 1",
                     desc: "Demo game created with my game framework CXGF",
-                    tag: ["HTML", "CSS", "JavaScript", "CXGF - Codotronix Game Framework", "Game"],
+                    tags: ["HTML", "CSS", "JavaScript", "CXGF - Codotronix Game Framework", "Game"],
                     img: "",
                     url: ""
                 },
                 {
                     name: "Web 2",
                     desc: "A tank battle game to demonstrate the browser responsiveness of AngularJS, where each tank is an angular directive.",
-                    tag: ["AngularJS", "HTML", "CSS", "JavaScript", "Game"],
+                    tags: ["AngularJS", "HTML", "CSS", "JavaScript", "Game"],
                     img: "",
                     url: ""
                 },
                 {
                     name: "Web 3",
                     desc: "A simple browser based game where the player needs to shoot down all the asteroids to save the day",
-                    tag: ["HTML", "CSS", "JavaScript", "Game"],
+                    tags: ["HTML", "CSS", "JavaScript", "Game"],
                     img: "",
                     url: ""
                 },
                 {
                     name: "Web 4",
                     desc: "A simple browser based game where the player needs to shoot down all the asteroids to save the day",
-                    tag: ["HTML", "CSS", "JavaScript", "Game"],
+                    tags: ["HTML", "CSS", "JavaScript", "Game"],
                     img: "",
                     url: ""
                 }
--- src/app/components/listcard/listcard.component.ts
+++ src/app/components/listcard/listcard.component.ts
@@ -1,25 +1,49 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'list-card',
+    selector: 'list-card',
 
-  template: `
+    template: `
     <div class="list-card">
-      item = {{item.name}}
+        <div class="title">{{item.name}}</div>
+        <div class="desc">{{item.desc}}</div>
+        <div class="tags">
+            <span *ngFor="let tag of item.tags" class="tag">
+                {{tag}}
+            </span>
+        </div>
     </div>
-  `,
 
-  styles: [
-  `
+    `,
+
+    styles: [
+    `
     .list-card {
-        border: 1px solid #00f;
+        border-bottom: 1px solid steelblue;
+        padding: 10px 15px 15px;
+    }
+    .list-card .title {
+        font-size: 22px;
+        font-weight: bold;
+    }
+    .list-card .desc {
+        font-size: 16px;
+    }
+    .list-card .tags .tag {
+        display: inline-block;
+        padding: 2px 5px;
+        margin: 1px 3px;
+        background: steelblue;
+        color: #fff;
+        font-size: 12px;
+        border-radius: 3px;
     }
 
-  `
-  ]
+    `
+    ]
 })
 
 
 export class ListCardComponent {
-  @Input() item;
+    @Input() item;
 }
